593bd6c7d043f36eba1e45652f5fa360400ad665
mounikapujari <mounikapujari2005@gmail.com>
2024-12-08 22:47:12 +0530

    Add files via upload

---
+++ Array2pointertech.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Array2pointertech {
+    public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+        arr[i]=s.nextInt(); 
+        int l=0;
+        int h=n-1;
+        while(l<=h){
+            int temp=arr[l];
+            arr[l]=arr[h];
+            arr[h]=temp;
+            l++;
+            h--;
+        }
+    }System.out.println(Arrays.toString(arr));
+}
+}
